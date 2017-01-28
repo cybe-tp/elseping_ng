356441a289ab4acc691a7bb79bfe9f0d29f9371c
Cybe <cybe@toppoint.de>
2017-01-28 17:42:05 +0100

    some spaces

--- tasks/views.py
+++ tasks/views.py
@@ -7,14 +7,14 @@ from . import models
 
 # Create your views here.
 def index(request):
-    return render(request,"tasks/index.html")
+    return render(request, "tasks/index.html")
 
 def select(request):
     next_task = models.Task.objects.filter(next_repeat__lte=Now()).order_by('next_repeat').first()
     if next_task is not None:
         next_task.last_complete = now()
         next_task.save()
-        return render(request,"tasks/select.html",context={
+        return render(request, "tasks/select.html", context={
             'next_task': next_task
         })
     else:
